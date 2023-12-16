798abf80b11830d5b3ad9e6175362bb4d69d7c81
windindicator <149794133+windindicator@users.noreply.github.com>
2023-12-16 21:31:43 +0800

    Update local_optimal_algorithm.py

--- local_optimal_algorithm.py
+++ local_optimal_algorithm.py
@@ -7,23 +7,24 @@ def get_smallest_cycle(tem_graph):
     for i in range(node_num):
         for j in range(node_num):
             if graph[i][j] == 0:
-                graph[i][j] = node_num + 2
-    result = node_num + 2
-    changed_graph = graph
+                graph[i][j] = 10000
+    result = 10000
+    changed_graph = graph.copy()
     for k in range(node_num):
         for i in range(k):
             for j in range(i + 1, k):
-                result = min(result, changed_graph[i][j] + graph[i][k] + graph[j][k])
-        for i in range(k):
-            for j in range(k):
-                changed_graph[i][j] = min(changed_graph[i][j], changed_graph[i][k] + changed_graph[j][k])
+                if result > changed_graph[i][j] + graph[i][k] + graph[k][j]:
+                    result = changed_graph[i][j] + graph[i][k] + graph[k][j]
+        for i in range(node_num):
+            for j in range(node_num):
+                changed_graph[i][j] = min(changed_graph[i][j], changed_graph[i][k] + changed_graph[k][j])
     result = int(result)
     if result > node_num:
         return 0, []
     else:
         visited_list = []
         for i in range(node_num):
-            ans, cycle_list = find_cycle(graph, i, i,  result, visited_list)
+            ans, cycle_list = find_cycle(graph, i, i, result, visited_list)
             if ans:
                 return result, cycle_list
 
@@ -105,15 +106,15 @@ def no_cycle_coloring(graph, node_list, c1, c2, color):
                 if node not in colored_node:
                     next_node_list = [node]
                     break
-        coloring_node_list = next_node_list.copy()
+        coloring_node_list = np.array(next_node_list)
         next_node_list = []
         for node in coloring_node_list:
             color[node] = next_color
-            print(node)
             colored_node.append(node)
             color_number += 1
             for child in node_list:
-                if graph[node][child] == 1 and child not in colored_node and child not in next_node_list:
+                if graph[node][
+                    child] == 1 and child not in colored_node and child not in next_node_list and child not in coloring_node_list:
                     next_node_list.append(child)
         if next_color == c1:
             next_color = c2
@@ -136,6 +137,7 @@ def local_optimal_coloring_algorithm(graph):
     tem_graph = graph.copy()
     remain_node_list = []
     max_color_number = 2
+    print(graph[69][69])
     color = []
     for i in range(len(graph[0])):
         color.append(0)
@@ -156,10 +158,14 @@ def local_optimal_coloring_algorithm(graph):
             row = tem_graph[i].copy()
             tem_graph[i] = [j for num, j in enumerate(row) if num not in cycle_node_index_list]
         cycle_list.append(cycle_node_list)
-        print(cycle_node_list)
     no_cycle_subgraph = remain_node_list
     main_sub_graph = no_cycle_subgraph
     color = no_cycle_coloring(graph, no_cycle_subgraph, 1, 2, color)
+    for i in tem_graph:
+        print(i)
+    print(tem_graph[10][10])
+    print(remain_node_list)
+    return 0,0
     for cycle in cycle_list:
         main_sub_graph = main_sub_graph + cycle
         print(len(main_sub_graph))
@@ -186,6 +192,7 @@ def local_optimal_coloring_algorithm(graph):
                         for color3 in range(1, max_color_number + 1):
                             if color1 == color2 or color1 == color3 or color2 == color3:
                                 continue
+                            print(color1, color2, color3)
                             color = odd_cycle_coloring(graph, cycle, color1, color2, color3, color)
                             is_valid = check_valid(graph, main_sub_graph, color)
                             if is_valid:
